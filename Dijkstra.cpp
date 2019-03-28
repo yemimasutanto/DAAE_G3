d59d927fc58b55e339db4e3577fc58a1cc7af8ba
elknhns <33386421+elknhns@users.noreply.github.com>
2019-03-28 19:20:19 +0700

    FIX

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -14,26 +14,19 @@ void graph(int arr[100][100], int vertex, int edge){
 		scanf("%d", &to);
 		printf("Weight Edge nya: ");
 		scanf("%d", &weight);
-		//scanf("%d%d%d", &from, &to, &weight); //input dari mana kemana sama weightnya
-		if(from>vertex || to>vertex || from<1 || to<1)
-		{
+		if(from>vertex || to>vertex || from<1 || to<1){
 			printf("Input errorr \n");
 			i--;
 		}
-		else
-		{
+		else{
 			arr[from-1][to-1]=weight;
 		}
-		
-		
-		//v[from].push_back(make_pair(to, weight)); //karena array dimulai dari 0 makanya -1
-
 	}
 }
 
 
 void dijkstra (int arr[100][100], int vertex, int edge){ //fungsi dijkstranya
-	int i, j, pilih, temp;
+	int i, j, pilih, temp, akhir;
 	int cek[100];    //ini buat cek kalo jaraknya lebih kecil di push ke queuenya
 	int hasil[100];  //ini hasil nanti diprint
 	priority_queue < pair <int, pair<vector<int>, int> > > dijkstra; //buat mencari jarak terpendek
@@ -51,6 +44,8 @@ void dijkstra (int arr[100][100], int vertex, int edge){ //fungsi dijkstranya
 	}
 	printf("\nPilih titik awal : ");
 	scanf("%d", &pilih);  //buat ngasi titik awal
+	printf("Pilih titik akhir: ");
+	scanf("%d", &akhir);
 	hasil[pilih-1]=arr[pilih-1][pilih-1];
 	for (i=0; i<vertex; i++){   //ini starter pack
 		if(i==pilih-1){
@@ -86,79 +81,80 @@ void dijkstra (int arr[100][100], int vertex, int edge){ //fungsi dijkstranya
 		}
 	}
 	printf("Maka hasilnya\n");
-	for(i=0; i<vertex; i++){ //dari sini kebawah print jarak terdekat beserta rutenya
-		if (hasil[i]==MAX){
+	if (hasil[akhir-1]==MAX){
 			printf("Dari titik %d ke titik %d tidak mempunyai jarak \n", pilih, i+1);
 		}
-		else{
-			printf("Dari titik %d ke titik %d jarak terdekatnya adalah %d \n", pilih, i+1, hasil[i]);
-			printf("Dengan rute dari: %d -> ", pilih);
-			int x;
-			if(i+1!=pilih){
-				for(x=0; x<rute[i].size(); x++){
-					if(x!=rute[i].size()-1){
-						printf("%d -> ", rute[i].at(x)+1);
-					}
-					else{
-						printf("%d\n\n", rute[i].at(x)+1);
-					}
-
+	else{
+		printf("Dari titik %d ke titik %d jarak terdekatnya adalah %d \n", pilih, akhir, hasil[akhir-1]);
+		printf("Dengan rute dari: %d -> ", pilih);
+		int x;
+		if(akhir!=pilih){
+			for(x=0; x<rute[akhir-1].size(); x++){
+				if(x!=rute[akhir-1].size()-1){
+					printf("%d -> ", rute[akhir-1].at(x)+1);
+				}
+				else{
+					printf("%d\n\n", rute[akhir-1].at(x)+1);
 				}
 			}
-			else{
-				printf("%d\n\n", pilih);
-			}
 		}
+		else printf("%d\n\n", pilih);
 	}
 }
 
 int main(){
-	int vertex, edge, i, j, from, to, weight, a, start;
-	printf("Welcome!\n");
-	printf("1. Mulai\n2. Keluar\n");
-	while(1){
-		scanf("%d",&start);
-		if (start==2) return 0;
-		else if (start==1) break;
-		else printf("Input error\n");
-	}
-	
-	while(1)
-	{
-		printf("Masukkan jumlah vertex (Jumlah vertex harus lebih dari 1):\n");
-		scanf("%d", &vertex);
-		if (vertex<2)
-		{
-			printf("Jumlah vertex anda tidak valid\n");
-		}
-		else
-		{
-			break;
-		}
-	}
-	
-	while(1)
-	{
-		printf("Masukkan jumlah edge (Jumlah Edge harus lebih dari 0):\n");
-		scanf("%d", &edge);	
-		if(edge<1)
-		{
-			printf("Jumlah edge anda tidak valid \n");
-		}
-		else
-		{
-			break;
-		}
-	}
-	
+	int vertex, edge, i, j, from, to, weight, a, start, hitung;
 	int arr[100][100];
-	printf("Vertex diberi nama 1 sampai jumlah vertexnya \n");
-	//printf("Masukkan jarak antar vertex dengan format \"dari\" \"ke\" dan \"weight\" !\n");
-	for(i=0; i<vertex; i++){
-		for(j=0; j<vertex; j++){
-			arr[i][j]=0;
+	hitung=0;
+	printf("Welcome!\n");
+			printf("1. Mulai\n2. Keluar\n");
+			while(1){
+				scanf("%d",&start);
+				if (start==2) return 0;
+				else if (start==1) break;
+				else printf("Input error\n");
+			}
+	while(1){
+		while(1){
+			printf("Masukkan jumlah vertex (Jumlah vertex harus lebih dari 1):\n");
+			scanf("%d", &vertex);
+			if (vertex<2){
+				printf("Jumlah vertex anda tidak valid\n");
+			}
+			else{
+				break;
+			}
+		}
+
+		while(1){
+			printf("Masukkan jumlah edge (Jumlah Edge harus lebih dari 0):\n");
+			scanf("%d", &edge);
+			if(edge<1){
+				printf("Jumlah edge anda tidak valid \n");
+			}
+			else{
+				break;
+			}
+		}
+		printf("Vertex diberi nama 1 sampai jumlah vertexnya \n");
+		for(i=0; i<vertex; i++){
+			for(j=0; j<vertex; j++){
+				arr[i][j]=0;
+			}
+		}
+		graph(arr, vertex, edge);
+		dijkstra(arr, vertex, edge);
+		while(1){
+			printf("Welcome!\n");
+			printf("1. Mulai Lagi\n2. Keluar\n3. Lanjut\n");
+			while(1){
+				scanf("%d",&start);
+				if (start==2) return 0;
+				else if (start==1) break;
+				else if (start==3) {dijkstra(arr, vertex, edge); break;}
+				else printf("Input error\n");
+			}
+			if(start==1) break;
 		}
 	}
-	graph(arr, vertex, edge);
-	dijkstra(arr, vertex, edge);
 }
